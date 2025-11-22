8f21924ca202707389736795c439a4de45d3bd2d
Michael Fleming <michael.fleming@accesso.com>
2025-11-22 17:42:02 -0500

    more
    distortion filter fixes
    bg image passed in

--- js/controllers/mainController.js
+++ js/controllers/mainController.js
@@ -1,32 +1,42 @@
 angular.module('liveWindowApp')
-    .controller('MainController', ['$scope', '$location', 'DisplayService', function($scope, $location, DisplayService) {
+    .controller('MainController', ['$scope', '$location', 'DisplayService', function ($scope, $location, DisplayService) {
         $scope.leftDisplay = DisplayService.getLeftDisplay();
         $scope.rightDisplay = DisplayService.getRightDisplay();
         $scope.editButtonHidden = false; // Track if edit button is hidden
-        
+
         // Navigation function
-        $scope.goToEdit = function() {
+        $scope.goToEdit = function () {
             $location.path('/edit');
         };
-        
+
         // Function to hide the edit button temporarily
-        $scope.hideEditButton = function() {
+        $scope.hideEditButton = function () {
             $scope.editButtonHidden = true;
         };
-        
+
+        $scope.getOverlayUrl = function (display) {
+            if (!display || !display.overlay) return '';
+            var url = display.overlay;
+            // Append background image if present so overlay can distort it
+            if (display.type === 'image' && display.content) {
+                url += '?bg=' + encodeURIComponent(display.content);
+            }
+            return url;
+        };
+
         // Watch for changes in the display service
-        $scope.$watch(function() {
+        $scope.$watch(function () {
             return DisplayService.getLeftDisplay();
-        }, function(newVal) {
+        }, function (newVal) {
             $scope.leftDisplay = newVal;
         }, true);
-        
-        $scope.$watch(function() {
+
+        $scope.$watch(function () {
             return DisplayService.getRightDisplay();
-        }, function(newVal) {
+        }, function (newVal) {
             $scope.rightDisplay = newVal;
         }, true);
-        
+
         // Initialize displays
         DisplayService.initializeDisplays();
     }]);
--- overlays/overlay-common.js
+++ overlays/overlay-common.js
@@ -1,40 +1,80 @@
 /* overlay-common.js
  * Small helper for overlays to lazy-load scripts and detect WebGL support.
  */
-(function(global){
+(function (global) {
   // Simple request dedupe map
   var _loadPromises = Object.create(null);
 
-  function loadScript(url){
+  function loadScript(url) {
     if (_loadPromises[url]) return _loadPromises[url];
-    var p = new Promise(function(resolve, reject){
+    var p = new Promise(function (resolve, reject) {
       var s = document.createElement('script'); s.src = url; s.async = true;
-      s.onload = function(){ resolve(); };
-      s.onerror = function(e){ reject(e); };
+      s.onload = function () { resolve(); };
+      s.onerror = function (e) { reject(e); };
       document.head.appendChild(s);
     });
     _loadPromises[url] = p;
     return p;
   }
 
-  function supportsWebGL(){
-    try{
+  function supportsWebGL() {
+    try {
       var c = document.createElement('canvas');
       return !!(window.WebGLRenderingContext && (c.getContext('webgl') || c.getContext('experimental-webgl')));
-    }catch(e){ return false; }
+    } catch (e) { return false; }
   }
 
   // Prefer a local vendor three file if present, otherwise fall back to CDN
-  function loadThree(){
+  function loadThree() {
     if (window.THREE) return Promise.resolve();
     var local = 'overlays/vendor/three.min.js';
     // Try loading local file first
-    return loadScript(local).catch(function(){
+    return loadScript(local).catch(function () {
       // Fallback to CDN
       var cdn = 'https://unpkg.com/three@0.158.0/build/three.min.js';
       return loadScript(cdn);
     });
   }
 
-  global.OverlayCommon = global.OverlayCommon || { loadScript: loadScript, supportsWebGL: supportsWebGL, loadThree: loadThree };
+  function getQueryParams() {
+    var params = {};
+    var search = window.location.search.substring(1);
+    if (!search) return params;
+    var pairs = search.split('&');
+    for (var i = 0; i < pairs.length; i++) {
+      var pair = pairs[i].split('=');
+      params[decodeURIComponent(pair[0])] = decodeURIComponent(pair[1] || '');
+    }
+    return params;
+  }
+
+  function loadBackgroundTexture() {
+    var params = getQueryParams();
+    if (!params.bg) return Promise.resolve(null);
+
+    // Ensure THREE is loaded
+    return loadThree().then(function () {
+      return new Promise(function (resolve) {
+        // Fix: Ensure path is absolute if it's a relative path (doesn't start with / or http)
+        var bgPath = params.bg;
+        if (bgPath && !bgPath.startsWith('/') && !bgPath.startsWith('http')) {
+          bgPath = '/' + bgPath;
+        }
+
+        new THREE.TextureLoader().load(bgPath, function (tex) {
+          resolve(tex);
+        }, undefined, function () {
+          resolve(null);
+        });
+      });
+    });
+  }
+
+  global.OverlayCommon = global.OverlayCommon || {
+    loadScript: loadScript,
+    supportsWebGL: supportsWebGL,
+    loadThree: loadThree,
+    getQueryParams: getQueryParams,
+    loadBackgroundTexture: loadBackgroundTexture
+  };
 })(window);
